a70da2d2ce8d8a84577d4ccccf50938c65ab064a
Jen <jen.lijo@zalando.de>
2018-07-23 21:13:59 +0200

    Add missing files

--- week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
+++ week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
@@ -1,3 +1,6 @@
+//Create a program that asks the user to input words, until the user gives an empty string.
+// Then the program prints the words the user gave in reversed order, the last word is printed first etc.
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -5,6 +8,24 @@ import java.util.Scanner;
 public class WordsInReverseOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // create here an ArrayList
+        ArrayList<String> arr = new ArrayList<String>();
+
+        while (true) {
+            System.out.println("Type a word: ");
+            String input = reader.nextLine();
+
+            if (input.equals("")) break;
+
+            arr.add(input);
+        }
+
+        int i = arr.size() - 1;
+
+        while (i >= 0) {
+            System.out.println(
+                    arr.get(i)
+            );
+            i--;
+        }
     }
 }
