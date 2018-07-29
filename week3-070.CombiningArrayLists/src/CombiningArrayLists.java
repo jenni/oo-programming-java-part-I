5ccdddf7daef41263aee2d351e4e9b0b9db1f59f
Jen <jen.lijo@zalando.de>
2018-07-29 10:26:18 +0200

    3.70 & 3.71

--- week3-070.CombiningArrayLists/src/CombiningArrayLists.java
+++ week3-070.CombiningArrayLists/src/CombiningArrayLists.java
@@ -1,3 +1,5 @@
+//Create the method public static void combine(ArrayList<Integer> first, ArrayList<Integer> second)
+// that inserts the items in a list called second to a list called first.
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -6,28 +8,21 @@ public class CombiningArrayLists {
 
     public static void main(String[] args) {
 
-        // You can change the values while testing
         ArrayList<Integer> list1 = new ArrayList<Integer>();
         ArrayList<Integer> list2 = new ArrayList<Integer>();
 
         Collections.addAll(list1, 4, 3);
-        // A shorthand for:
-        // list1.add(4);
-        // list1.add(3);
-
-
         Collections.addAll(list2, 5, 10, 7);
-        // A shorthand for:
-        // list2.add(5);
-        // list2.add(10);
-        // list2.add(7);
 
-        // Implement the following method and then remove the comment
-        // combine(list1, list2);
+        combine(list1, list2);
         System.out.println(list1);
         System.out.println(list2);
+    }
 
-
+    public static void combine(
+            ArrayList<Integer> first,
+            ArrayList<Integer> second) {
+        first.addAll(second);
     }
 
 }
--- week3-071.SmartCombining/src/SmartCombining.java
+++ week3-071.SmartCombining/src/SmartCombining.java
@@ -1,3 +1,6 @@
+//Create the method smartCombine that works like the previous combine method
+// except that numbers can be on the list only once.
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -8,13 +11,20 @@ public class SmartCombining {
         ArrayList<Integer> list2 = new ArrayList<Integer>();
 
         Collections.addAll(list1, 4, 3);
-
         Collections.addAll(list2, 5, 10, 4, 3, 7);
 
-        // remove comment when method ready
-        //smartCombine(list1, list2);
+        smartCombine(list1, list2);
         System.out.println(list1);
         System.out.println(list2);
     }
 
+    public static void smartCombine(
+            ArrayList<Integer> first,
+            ArrayList<Integer> second) {
+
+        for (int num : second) {
+            if (first.contains(num)) continue;
+            first.add(num);
+        }
+    }
 }
