6df08c027758770f1ab85d578c23bb7fb6242ff5
Jen <jen.lijo@zalando.de>
2018-08-09 21:05:00 +0200

    Week 4: 75

--- week4-075.DecreasingCounter/src/DecreasingCounter.java
+++ week4-075.DecreasingCounter/src/DecreasingCounter.java
@@ -1,8 +1,24 @@
+// The constructor of DecreasingCounter receives as parameter the initial value of the counter.
+//
+//Exercise 75.1: Implementing method decrease()
+//Implement the method decrease() so that when called, the object variable this.value is decreased by one.
+//
+//Exercise 75.2: Value remains positive
+//Change your implementation of the method decrease() so that the value of the counter will not drop below zero.
+//
+//Exercise 75.3: Counter reset
+//Implement the method public void reset() that sets the value of the counter to zero.
+//
+//Exercise 75.4: Back to initial value
+//Implement the method public void setInitial(), which returns the counter to its initial value.
+
 public class DecreasingCounter {
-    private int value;  // instance variable that remembers the value of the counter
+    private int value;
+    private int initialValue;
 
     public DecreasingCounter(int valueAtStart) {
         this.value = valueAtStart;
+        this.initialValue = valueAtStart;
     }
 
     public void printValue() {
@@ -10,8 +26,14 @@ public class DecreasingCounter {
     }
 
     public void decrease() {
-        // write here code to decrease counter value by one
+        if (this.value > 0) this.value--;
     }
 
-    // and here the rest of the methods
+    public void reset() {
+        this.value = 0;
+    }
+
+    public void setInitial() {
+        this.value = initialValue;
+    }
 }
--- week4-075.DecreasingCounter/src/Main.java
+++ week4-075.DecreasingCounter/src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        DecreasingCounter counter = new DecreasingCounter(10);
+        DecreasingCounter counter = new DecreasingCounter(1);
 
         counter.printValue();
         counter.decrease();
+        counter.printValue();
         counter.decrease();
+        counter.printValue();
     }
 }
