94dfa66aa1cc5043265e2de832d0af75b3cb8c00
Jen <jen.lijo@zalando.de>
2018-08-10 08:16:21 +0200

    Week 4 - 78

--- week4-078.ClockUsingCounter/src/Main.java
+++ week4-078.ClockUsingCounter/src/Main.java
@@ -5,9 +5,32 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // write here code to ensure that BoundedCounter works as expected
-        // before starting 78.3 remove the extra code and use the skeleton shown
-        // in the assignment
+        BoundedCounter seconds = new BoundedCounter(59);
+        BoundedCounter minutes = new BoundedCounter(59);
+        BoundedCounter hours = new BoundedCounter(23);
 
+        System.out.print("seconds: ");
+        int s = 50;
+
+        System.out.print("minutes: ");
+        int m = 59;
+
+        System.out.print("hours: ");
+        int h = 23;
+
+        seconds.setValue(s);
+        minutes.setValue(m);
+        hours.setValue(h);
+
+        int i = 0;
+        while ( i < 121 ) {
+            System.out.println( hours + ":" + minutes + ":" + seconds);
+            seconds.next();
+            if (seconds.getValue() == 0)
+                minutes.next();
+            if (seconds.getValue() == 0 && minutes.getValue() == 0)
+                hours.next();
+            i++;
+        }
     }
 }
