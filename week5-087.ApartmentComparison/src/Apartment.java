6c444ceb44045934064156fc6486e1829eebb2dd
Jen <jen.lijo@zalando.de>
2018-08-12 08:00:00 +0200

    week5-087

--- week5-087.ApartmentComparison/src/Apartment.java
+++ week5-087.ApartmentComparison/src/Apartment.java
@@ -10,5 +10,22 @@ public class Apartment {
         this.squareMeters = squareMeters;
         this.pricePerSquareMeter = pricePerSquareMeter;
     }
-    
+
+    public boolean larger(Apartment apartment) {
+        if (this.squareMeters > apartment.squareMeters) return true;
+        return false;
+    }
+
+    public int priceDifference(Apartment apartment) {
+        return Math.abs(this.totalPrice() - apartment.totalPrice());
+    }
+
+    public boolean moreExpensiveThan(Apartment apartment) {
+        if (this.totalPrice() > apartment.totalPrice()) return true;
+        return false;
+    }
+
+    private int totalPrice() {
+        return this.pricePerSquareMeter * this.squareMeters;
+    }
 }
