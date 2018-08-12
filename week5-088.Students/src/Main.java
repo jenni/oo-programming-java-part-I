d112efdb8829562f9f38e65b6785abb4b1ff5a2f
Jen <jen.lijo@zalando.de>
2018-08-12 08:50:40 +0200

    week5-88

--- week5-088.Students/src/Main.java
+++ week5-088.Students/src/Main.java
@@ -5,6 +5,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write here the main program
+        Scanner reader = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<Student>();
+        String name = "";
+        String number = "";
+
+        while (true) {
+            System.out.println("name: ");
+            name = reader.nextLine();
+            if (name.isEmpty()) break;
+            System.out.println("studentnumber: ");
+            number = reader.nextLine();
+            Student student = new Student(name, number);
+
+            students.add(student);
+        }
+
+//        students.forEach(student -> System.out.println(student));
+        for (Student student : students) System.out.println(student);
+
+        System.out.println("Give search term: ");
+        String term = reader.nextLine();
+
+        System.out.println("Result: ");
+        for (Student student : students)
+            if (student.getName().contains(term)) System.out.println(student);
     }
 }
