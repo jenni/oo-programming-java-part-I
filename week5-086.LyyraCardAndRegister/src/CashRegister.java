0b85e710d1727e8a8aa383a21a3ff2d47ff20b35
Jen <jen.lijo@zalando.de>
2018-08-12 07:24:29 +0200

    week5-086

--- week5-086.LyyraCardAndRegister/src/CashRegister.java
+++ week5-086.LyyraCardAndRegister/src/CashRegister.java
@@ -5,32 +5,62 @@ public class CashRegister {
     private int economicalSold;
     private int gourmetSold;
 
+    private static final double PRICE1 = 2.50;
+    private static final double PRICE2 = 4.00;
+
     public CashRegister() {
-        // at start the register has 1000 euros 
+        this.cashInRegister = 1000;
+        this.economicalSold = 0;
+        this.gourmetSold = 0;
     }
 
     public double payEconomical(double cashGiven) {
-        // price of the economical lunch is 2.50 euros
-        // if the given cash is at least the price of the lunch:
-        //    the price of lunch is added to register
-        //    the amount of sold lunch is incremented by one
-        //    method returns cashGiven - lunch price 
-        // if not enough money given, all is returned and nothing else happens        
-        return -1;
+        if (cashGiven >= PRICE1) {
+            this.cashInRegister += PRICE1;
+            this.economicalSold++;
+            return cashGiven - PRICE1;
+        }
+        return cashGiven;
     }
+
+    public boolean payEconomical(LyyraCard card) {
+        if (card.balance() >= PRICE1) {
+            card.pay(PRICE1);
+            this.economicalSold++;
+            return true;
+        }
+
+        return false;
+    }
+
 
     public double payGourmet(double cashGiven) {
-        // price of the gourmet lunch is 4.00 euros
-        // if the given cash is at least the price of the lunch:
-        //    the price of lunch is added to register
-        //    the amount of sold lunch is incremented by one
-        //    method returns cashGiven - lunch price 
-        // if not enough money given, all is returned and nothing else happens
-        return -1;
+        if (cashGiven >= PRICE2) {
+            this.cashInRegister += PRICE2;
+            this.gourmetSold++;
+            return cashGiven - PRICE2;
+        }
+        return cashGiven;
     }
 
+    public boolean payGourmet(LyyraCard card) {
+        if (card.balance() >= PRICE2) {
+            card.pay(PRICE2);
+            this.gourmetSold++;
+            return true;
+        }
+
+        return false;
+    }
 
     public String toString() {
         return "money in register " + cashInRegister + " economical lunches sold: " + economicalSold + " gourmet lunches sold: " + gourmetSold;
+    }
+
+    public void loadMoneyToCard(LyyraCard card, double sum) {
+        if (sum > 0) {
+            card.loadMoney(sum);
+            this.cashInRegister += sum;
+        }
     }
 }
