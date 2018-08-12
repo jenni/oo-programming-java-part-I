eceed4070f05cc4586a8bb319f4b83151fece22b
Jen <jen.lijo@zalando.de>
2018-08-12 12:00:43 +0200

    week5-089

--- week5-089.ClockObject/src/BoundedCounter.java
+++ week5-089.ClockObject/src/BoundedCounter.java
@@ -1,4 +1,31 @@
-
 public class BoundedCounter {
-    // copy here the class BoundedCounter from last weeks assignment 78 
+    private int value;
+    private int upperLimit;
+
+    public BoundedCounter(int upperLimit) {
+        this.value = 0;
+        this.upperLimit = upperLimit;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(int n) {
+        if (n > 0 && n <= upperLimit)
+            this.value = n;
+    }
+
+    public void next() {
+        this.value++;
+        if (this.value > upperLimit)
+            this.value = 0;
+    }
+
+    public String toString() {
+        if (this.value < 10)
+            return "" + 0 + this.value;
+        else
+            return "" + this.value;
+    }
 }
