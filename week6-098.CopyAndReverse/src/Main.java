79842e4540924637b08761f117395c07e433294f
Jen <jen.lijo@zalando.de>
2018-09-02 10:05:08 +0200

    week6-098.CopyAndReverse

--- week6-098.CopyAndReverse/src/Main.java
+++ week6-098.CopyAndReverse/src/Main.java
@@ -1,9 +1,30 @@
-
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write testcode here
+        int[] original = {1, 2, 3, 4};
+        int[] reverse = reverseCopy(original);
+
+        // print both
+        System.out.println( "original: " +Arrays.toString(original));
+        System.out.println( "reversed: " +Arrays.toString(reverse));
+    }
+
+    public static int[] copy(int[] array) {
+        return array.clone();
+    }
+
+    public static int[] reverseCopy(int[] array) {
+        int[] reversed = new int[array.length];
+
+        int index = 0;
+        for (int i = array.length -1; i >= 0; i--) {
+            reversed[index] = array[i];
+            index++;
+        }
+
+        return reversed;
     }
     
 }
