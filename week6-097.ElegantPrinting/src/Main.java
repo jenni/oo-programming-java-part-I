ec0bf3fa4e8693d3de3a210b777a95ec85a4b1c4
Jen <jen.lijo@zalando.de>
2018-09-02 10:04:51 +0200

    week6-097.ElegantPrinting

--- week6-097.ElegantPrinting/src/Main.java
+++ week6-097.ElegantPrinting/src/Main.java
@@ -2,12 +2,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        // test method here
         int[] array = {5, 1, 3, 4, 2};
         printElegantly(array);
     }
 
     public static void printElegantly(int[] array) {
-        // Write code here
+
+        int i = 0;
+        while (i < array.length) {
+            if (i == array.length -1) {
+                System.out.println(array[i]);
+                break;
+            }
+            System.out.print(array[i] + ", ");
+            i++;
+        }
     }
 }
